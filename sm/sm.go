c7bd5ea1ec51bc61dae13fbb4fe15b0b8ae3b479
BS <cooltech.bs@gmail.com>
2019-02-04 09:12:25 +0800

    Go mapping of enums and structs

--- sm/sm.go
+++ sm/sm.go
@@ -4,3 +4,89 @@ package sm
 // #cgo CXXFLAGS: -DWHATEVER_YOU_WANT_TO_INDICATE_CGO=1
 // #include "sm.h"
 import "C"
+
+import (
+	"errors"
+)
+
+// Enum: kusokurae_game_status_t
+const (
+	StatusNull = iota
+	StatusInit
+	StatusPlay
+	StatusFinish
+	StatusMax
+)
+
+// Enum: kusokurae_card_suit_t
+const (
+	SuitXiang   = -1
+	SuitYoutiao = 0
+	SuitBaozi   = 1
+	SuitOther   = 2
+)
+
+// Enum: kusokurae_round_status_t
+const (
+	RoundWaiting = iota
+	RoundActive
+	RoundDone
+)
+
+// Errors from underlying library.
+// Don't forget also to change here after adding new error codes in C interface.
+var (
+	ErrNullPtr       = errors.New("KUSOKURAE_ERROR_NULLPTR")
+	ErrBadNPlayers   = errors.New("KUSOKURAE_ERROR_BAD_NUMBER_OF_PLAYERS")
+	ErrUninitialized = errors.New("KUSOKURAE_ERROR_UNINITIALIZED")
+)
+
+var errMap = map[C.int]error{
+	C.KUSOKURAE_SUCCESS:                     nil,
+	C.KUSOKURAE_ERROR_NULLPTR:               ErrNullPtr,
+	C.KUSOKURAE_ERROR_BAD_NUMBER_OF_PLAYERS: ErrBadNPlayers,
+	C.KUSOKURAE_ERROR_UNINITIALIZED:         ErrUninitialized,
+}
+
+// GameConfig has the same memory layout with C.kusokurae_game_config_t.
+type GameConfig struct {
+	NumPlayers int
+}
+
+// Card has the same memory layout with C.kusokurae_card_t.
+type Card struct {
+	DisplayOrder uint
+	Suit         int // C.kusokurae_card_suit_t
+	Rank         int
+}
+
+// Player has the same memory layout with C.kusokurae_player_t.
+type Player struct {
+	Index      int
+	Active     int // C.kusokurae_round_status_t
+	Hand       [C.KUSOKURAE_MAX_HAND_CARDS]Card
+	NumCards   int
+	CardsTaken int
+	Score      int
+}
+
+// GameState has the same memory layout with C.kusokurae_game_state_t.
+type GameState struct {
+	cfg          GameConfig
+	status       int
+	players      [C.KUSOKURAE_MAX_PLAYERS]Player
+	activePlayer int
+	numRound     int
+	ghostHolder  int
+	curRound     [C.KUSOKURAE_MAX_PLAYERS]Card
+}
+
+// RoundState corresponds to C.kusokurae_round_state_t, but does not preserve
+// its memory layout - a bit of conversion needs to be done when providing this
+// to user Go code.
+type RoundState struct {
+	Seq          int
+	IsDoubled    bool
+	ScoreOnBoard int
+	Leader       *Player
+}
