496145001aeb580bd348fafe2dd96e5ce1479026
Chang Qian <cooltech-bs@users.noreply.github.com>
2019-02-05 16:16:41 +0800

    TestGameStart

--- sm/sm_test.go
+++ sm/sm_test.go
@@ -24,3 +24,32 @@ func TestNewGame(t *testing.T) {
 	assert.Equal(t, 3, int(state.players[2].Index))
 	assert.Equal(t, 0, int(state.players[3].Index))
 }
+
+func TestGameStart(t *testing.T) {
+	state, err := NewGame(GameConfig{
+		NumPlayers: 3,
+	})
+	assert.NoError(t, err)
+
+	err = state.Start()
+	assert.NoError(t, err)
+	assert.Equal(t, RoundActive, state.players[0].Active)
+	assert.Equal(t, RoundWaiting, state.players[1].Active)
+	assert.Equal(t, RoundWaiting, state.players[2].Active)
+	assert.Equal(t, StatusPlay, state.status)
+
+	t.Log(state.players[0].Hand)
+	t.Log(state.players[1].Hand)
+	t.Log(state.players[2].Hand)
+	var cards [33]bool
+	var i, j int
+	var order uint32
+	for i = 0; i < 3; i++ {
+		for j = 0; j < 11; j++ {
+			order = state.players[i].Hand[j].DisplayOrder - 1
+			if cards[order] {
+				t.Errorf("Duplicate card %+v", state.players[i].Hand[j])
+			}
+		}
+	}
+}
