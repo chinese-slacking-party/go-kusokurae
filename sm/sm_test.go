0021bc7b705dce70cac215f3bd955fddfb6c6eae
BS <cooltech.bs@gmail.com>
2019-02-05 09:57:33 +0800

    Finish TestNewGame

--- sm/sm_test.go
+++ sm/sm_test.go
@@ -11,4 +11,16 @@ func TestNewGame(t *testing.T) {
 		NumPlayers: 2,
 	})
 	assert.Equal(t, ErrBadNPlayers, err)
+
+	correctCfg := GameConfig{
+		NumPlayers: 3,
+	}
+	state, err := NewGame(correctCfg)
+	assert.NoError(t, err)
+	assert.Equal(t, correctCfg, state.cfg)
+	assert.Equal(t, StatusInit, state.status)
+	assert.Equal(t, 1, int(state.players[0].Index))
+	assert.Equal(t, 2, int(state.players[1].Index))
+	assert.Equal(t, 3, int(state.players[2].Index))
+	assert.Equal(t, 0, int(state.players[3].Index))
 }
