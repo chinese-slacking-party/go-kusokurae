ac9d62289daa5e04abb7d432c8693b9c1582a174
Chang Qian <cooltech-bs@users.noreply.github.com>
2019-02-24 17:02:24 +0800

    Refactor C++ test for later addition of command-line self-playing

--- sm/test.cxx
+++ sm/test.cxx
@@ -32,9 +32,7 @@ void print_card(kusokurae_card_t *p) {
     std::printf("%d\n", p->rank);
 }
 
-#ifndef WHATEVER_YOU_WANT_TO_INDICATE_CGO
-
-int main(int argc, char *argv[]) {
+void test_init() {
     std::printf("Starting kusokurae engine test @ %ld...\n\n", time(0));
     std::printf("Sizes of data structures:\n");
     std::printf("kusokurae_game_config_t: %lu\n", sizeof(kusokurae_game_config_t));
@@ -43,19 +41,29 @@ int main(int argc, char *argv[]) {
     std::printf("kusokurae_game_state_t: %lu\n", sizeof(kusokurae_game_state_t));
     std::printf("kusokurae_round_state_t: %lu\n", sizeof(kusokurae_round_state_t));
     kusokurae_global_init();
+}
+
+void test_start(kusokurae_game_state_t *g) {
+    int i, j;
+    kusokurae_game_start(g);
+    for (i = 0; i < 4; i++) {
+        std::printf("\n%dP's cards:\n", i + 1);
+        for (j = 0; j < 8; j++) {
+            print_card(&g->players[i].cards[j]);
+        }
+    }
+}
+
+#ifndef WHATEVER_YOU_WANT_TO_INDICATE_CGO
+
+int main(int argc, char *argv[]) {
+    test_init();
 
     kusokurae_game_config_t cfg = { 4 };
     kusokurae_game_state_t g;
     kusokurae_game_init(&g, &cfg);
 
-    int i, j;
-    kusokurae_game_start(&g);
-    for (i = 0; i < 4; i++) {
-        std::printf("\n%dP's cards:\n", i + 1);
-        for (j = 0; j < 8; j++) {
-            print_card(&g.players[i].cards[j]);
-        }
-    }
+    test_start(&g);
     std::printf("\n%dP has the ghost\n", g.ghost_holder_index + 1);
 }
 
