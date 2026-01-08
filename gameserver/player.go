f4674d4514ac0b4587fe59de41492cfecba4f202
muzhengyi <muzhengyi@gstianfu.com>
2026-01-08 21:23:25 +0800

    temp commit

--- gameserver/player.go
+++ gameserver/player.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/bs-iron-trio/go-kusokurae/sm"
+import "github.com/google/uuid"
 
 type Player struct {
-	ID string
-	*sm.Player
+	ID            string
+	RoomID        string
+	RoomPosistion int32
+}
+
+func NewPlayer() *Player {
+	u, err := uuid.NewRandom()
+	if err != nil {
+		panic("failed to generate player ID")
+	}
+	return &Player{
+		ID:            u.String(),
+		RoomID:        "",
+		RoomPosistion: -1,
+	}
+}
+
+func (p *Player) Sit(roomID string, roomPosition int32) {
+	p.RoomID = roomID
+	p.RoomPosistion = roomPosition
 }
--- gameserver/room.go
+++ gameserver/room.go
@@ -1,13 +1,47 @@
 package main
 
 import (
+	"errors"
+	"sync"
+
+	"github.com/bs-iron-trio/go-kusokurae/sm"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
 
+var ErrRoomFull = errors.New("room is full!!!")
+
 type Room struct {
-	ID               string
-	LasestActiveTime int64
+	ID                string
+	Mutex             sync.Mutex // protects the room state
+	LasestActiveTime  int64
+	RoomGameConfig    *sm.GameConfig
+	CurrentGame       *Game
+	CurrentPlayers    int32
+	Players           []*Player
+	PlayerReadyStatus []*int32
+}
+
+func NewRoom(id string, config *sm.GameConfig) *Room {
+	return &Room{
+		ID:                id,
+		RoomGameConfig:    config,
+		CurrentPlayers:    0,
+		Players:           make([]*Player, config.NumPlayers),
+		PlayerReadyStatus: make([]*int32, config.NumPlayers),
+	}
+}
+
+func (r *Room) AddPlayer(player *Player) error {
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+	if r.CurrentPlayers >= r.RoomGameConfig.NumPlayers {
+		return ErrRoomFull
+	}
+	r.Players[r.CurrentPlayers] = player
+	r.CurrentPlayers++
+	player.Sit(r.ID, r.CurrentPlayers-1)
+	return nil
 }
 
 var roomRepository map[string]*Room
@@ -18,14 +52,24 @@ func InitRoomRepository() {
 
 // 创建游戏房间
 func CreateRoom(ctx *gin.Context) {
+	var gameConfig sm.GameConfig
+	err := ctx.BindJSON(&gameConfig)
+	if gameConfig.NumPlayers != 3 && gameConfig.NumPlayers != 4 {
+		ctx.JSON(400, NewErrorRes(COMMON_ERR_CODE, "Invalid number of players"))
+		return
+	}
+
 	u, err := uuid.NewRandom()
 	if err != nil {
 		ctx.JSON(200, NewErrorRes(COMMON_ERR_CODE, err.Error()))
 		return
 	}
-	var roomID string = u.String()
-	// TODO 创建房间并仓库
-	ctx.JSON(200, NewSuccessRes(roomID))
+
+	var room = NewRoom(u.String(), &gameConfig)
+	var masterPlayer = NewPlayer()
+	room.AddPlayer(masterPlayer)
+	roomRepository[room.ID] = room
+	ctx.JSON(200, NewSuccessRes(room.ID))
 }
 
 // 加入游戏房间并生成userToken
