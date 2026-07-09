4ece08ba2342649d4dd6c8f847f43ad6d3b243a3
muzhengyi <muzhengyi@gstianfu.com>
2026-07-09 16:42:31 +0800

    feat: add Disconnected chan and Session ref to Player

--- gameserver/player.go
+++ gameserver/player.go
@@ -1,21 +1,17 @@
 package gameserver
 
 import (
-	"context"
-	"log"
-	"sync"
-
 	"github.com/google/uuid"
 )
 
 type Player struct {
-	ID            string
-	RoomID        string
+	ID           string
+	RoomID       string
 	RoomPosition int32
-	NoticeCh      chan Message
-	OperatorCh    chan Message
-	sync.Mutex
-	joystick Joystick
+	NoticeCh     chan Message
+	OperatorCh   chan Message
+	Disconnected chan struct{}
+	Session      *Session
 }
 
 func NewPlayer() *Player {
@@ -24,56 +20,16 @@ func NewPlayer() *Player {
 		panic("failed to generate player ID")
 	}
 	return &Player{
-		ID:            u.String(),
-		RoomID:        "",
+		ID:           u.String(),
+		RoomID:       "",
 		RoomPosition: -1,
-		NoticeCh:      make(chan Message),
-		OperatorCh:    make(chan Message),
-		joystick:      nil,
+		NoticeCh:     make(chan Message),
+		OperatorCh:   make(chan Message),
+		Disconnected: make(chan struct{}),
 	}
 }
 
 func (p *Player) Sit(roomID string, roomPosition int32) {
 	p.RoomID = roomID
 	p.RoomPosition = roomPosition
-}
-
-func (p *Player) getJoystick() Joystick {
-	p.Mutex.Lock()
-	defer p.Mutex.Unlock()
-	return p.joystick
-}
-
-func (p *Player) setJoystick(joystick Joystick) {
-	p.Mutex.Lock()
-	defer p.Mutex.Unlock()
-	p.joystick = joystick
-}
-
-func (p *Player) WriteControlSignalFn(ctx context.Context) {
-	for {
-		msg := <-p.NoticeCh
-		joystick := p.getJoystick()
-		if joystick == nil {
-			log.Println("discards msg: ", msg)
-		} else {
-			if err := p.joystick.WriteMessage(msg); err != nil {
-				log.Println("write msg into joystick error: ", err.Error())
-				p.setJoystick(nil)
-			}
-		}
-	}
-}
-
-func (p *Player) ReadControlSignalFn(ctx context.Context) {
-	for {
-		joystick := p.getJoystick()
-		msg, err := joystick.ReadMessage()
-		if err != nil {
-			log.Println("read msg from joystick error: ", err.Error())
-			p.setJoystick(nil)
-			break
-		}
-		p.OperatorCh <- msg
-	}
 }
