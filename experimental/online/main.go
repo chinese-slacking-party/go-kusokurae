40954ded709b6e9f4469ea1155cb9f94df18c9be
muzhengyi <muzhengyi@gstianfu.com>
2026-08-28 00:01:08 +0800

    debug 模式下添加性能分析器

--- experimental/online/main.go
+++ experimental/online/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http/pprof"
+	_ "net/http/pprof"
+
 	"github.com/gin-gonic/gin"
 
 	"github.com/bs-iron-trio/go-kusokurae/gameserver"
@@ -9,6 +12,9 @@ import (
 func main() {
 	gameserver.InitRoomRepository()
 	r := gin.Default()
+	if gin.Mode() != "release" {
+		r.GET("/debug/pprof/*any", gin.WrapF(pprof.Index))
+	}
 	r.POST("/api/v1/room/new", CreateRoom)
 	r.POST("/api/v1/room/join", JoinRoom)
 	r.GET("/api/v1/communication/:room_id/:player_id", handleWebSocket)
